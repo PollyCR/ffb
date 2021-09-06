98788d286c739bb203cb0b89b7276762d488a98d
Polly <polly.reynolds@newday.co.uk>
2021-09-06 14:18:59 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider } from "@apollo/client";
+import { BrandThemeProvider } from "@eggplant-ui/theme";
+import client from "./graphql";
+const brand = "aqua";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrandThemeProvider brand={brand}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrandThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
